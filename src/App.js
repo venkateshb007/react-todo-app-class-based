8ef5de5c32baed6e32493ba4a0b0e1b8bb88d443
Venkateshb007 <venkateshb698@gmail.com>
2022-11-22 12:51:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">ToDo App in ReactJS</h1>
+        <Todos />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/js/dist/modal";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
